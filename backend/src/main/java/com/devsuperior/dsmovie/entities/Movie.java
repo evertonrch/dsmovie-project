fbc8d010a090a5d13527f52e38b518320952185a
Everton Rocha <irocha10@hotmail.com>
2022-01-15 09:54:18 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,8 +1,19 @@
 package com.devsuperior.dsmovie.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
 public class Movie {
 
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
+    
     private String title;
     private Double score;
     private Integer count;
--- backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -1,8 +1,16 @@
 package com.devsuperior.dsmovie.entities;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
 public class Score {
 
+    @EmbeddedId
     private ScorePK id = new ScorePK();
+    
     private Double value;
 
     public Score() {}
